swaywm/swaylock

commit 11560381bf54d228aa09aa61bd31135ae0ea9662

Damage state when the keyboard layout changes

seat.c
@@ -62,6 +62,11 @@ static void keyboard_modifiers(void *data, struct wl_keyboard *wl_keyboard,
 		uint32_t serial, uint32_t mods_depressed, uint32_t mods_latched,
 		uint32_t mods_locked, uint32_t group) {
 	struct swaylock_state *state = data;
+	int layout_same = xkb_state_layout_index_is_active(state->xkb.state,
+		group, XKB_STATE_LAYOUT_EFFECTIVE);
+	if (!layout_same) {
+		damage_state(state);
+	}
 	xkb_state_update_mask(state->xkb.state,
 		mods_depressed, mods_latched, mods_locked, 0, 0, group);
 	int caps_lock = xkb_state_mod_name_is_active(state->xkb.state,

commit 0d83f69cd6df99b9e6a4d8c2b9760ec012b9a1f8

Display correct layout when keymap also changes
When there are two layouts (with IDs 0 and 1) and keyboard_modifiers()
is called right after keyboard_keymap(), existing code is not able to
see layout changing from 1 to 0 because keyboard_keymap() allocates new
state->xkb.state, thus resetting "effective layout" to 0.

This patch adds new variable in state->xkb to track layout change
independently of state->xkb.state.

Fixes: 11560381bf54 ("Damage state when the keyboard layout changes")

include/seat.h
@@ -5,6 +5,7 @@
 struct swaylock_xkb {
 	bool caps_lock;
 	bool control;
+	uint32_t layout;
 	struct xkb_state *state;
 	struct xkb_context *context;
 	struct xkb_keymap *keymap;

render.c
@@ -168,16 +168,7 @@ void render_frame(struct swaylock_surface *surface) {
 			xkb_layout_index_t num_layout = xkb_keymap_num_layouts(state->xkb.keymap);
 			if (!state->args.hide_keyboard_layout &&
 					(state->args.show_keyboard_layout || num_layout > 1)) {
-				xkb_layout_index_t curr_layout = 0;
-
-				// advance to the first active layout (if any)
-				while (curr_layout < num_layout &&
-					xkb_state_layout_index_is_active(state->xkb.state,
-						curr_layout, XKB_STATE_LAYOUT_EFFECTIVE) != 1) {
-					++curr_layout;
-				}
-				// will handle invalid index if none are active
-				layout_text = xkb_keymap_layout_get_name(state->xkb.keymap, curr_layout);
+				layout_text = xkb_keymap_layout_get_name(state->xkb.keymap, state->xkb.layout);
 			}
 			break;
 		default:

seat.c
@@ -62,9 +62,8 @@ static void keyboard_modifiers(void *data, struct wl_keyboard *wl_keyboard,
 		uint32_t serial, uint32_t mods_depressed, uint32_t mods_latched,
 		uint32_t mods_locked, uint32_t group) {
 	struct swaylock_state *state = data;
-	int layout_same = xkb_state_layout_index_is_active(state->xkb.state,
-		group, XKB_STATE_LAYOUT_EFFECTIVE);
-	if (!layout_same) {
+	if (group != state->xkb.layout) {
+		state->xkb.layout = group;
 		damage_state(state);
 	}
 	xkb_state_update_mask(state->xkb.state,