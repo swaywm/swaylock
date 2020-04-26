600477eade5dc77f14e34d99136ca7484da5aad5
Martin Dørum <martid0311@gmail.com>
2020-04-26 15:50:09 +0200

    add SSE implementation of fading

--- fade.c
+++ fade.c
@@ -2,16 +2,59 @@
 #include "pool-buffer.h"
 #include "swaylock.h"
 #include <stdio.h>
-#include <time.h>
 #include <omp.h>
+#include <stdalign.h>
 
-// TODO: optimize this with AVX instructions.
-// On my laptop, using 100% of all cores, it would barely be able to fade
-// one screen at 90FPS.
-// The reason this isn't just a simple "set the alpha byte of each pixel" operation
-// is that Wayland uses pre-multiplied alpha.
-static void set_alpha(uint32_t *orig, struct pool_buffer *buf, float alpha) {
-#pragma omp parallel for
+#ifdef FADE_PROFILE
+#include <time.h>
+double get_time() {
+	struct timespec tv;
+	clock_gettime(CLOCK_MONOTONIC, &tv);
+	return tv.tv_sec + (tv.tv_nsec / 1000000000.0);
+}
+#endif
+
+#ifdef __SSE2__
+#define set_alpha set_alpha_sse
+
+#include <immintrin.h>
+
+static void set_alpha_sse(uint32_t *orig, struct pool_buffer *buf, float alpha) {
+	int alpha_factor = (int)(alpha * (1 << 16));
+	if (alpha_factor != 0)
+		alpha_factor -= 1;
+
+	__m128i alpha_vec = _mm_set_epi16(
+			alpha_factor, alpha_factor, alpha_factor, alpha_factor,
+			alpha_factor, alpha_factor, alpha_factor, alpha_factor);
+	__m128i dummy_vec = _mm_set_epi16(0, 0, 0, 0, 0, 0, 0, 0);
+
+	uint8_t *orig_bytes = (uint8_t *)orig;
+	uint8_t *dest_bytes = (uint8_t *)buf->data;
+
+	for (size_t y = 0; y < buf->height; ++y) {
+		for (size_t rx = 0; rx < buf->width / 2; ++rx) {
+			size_t x = rx * 2;
+			size_t index = (y * buf->width + x) * 4;
+
+			// Read data into SSE register, where each byte is an u16
+			__m128i argb_vec = _mm_loadu_si64(orig_bytes + index);
+			argb_vec = _mm_unpacklo_epi8(argb_vec, dummy_vec);
+
+			// Multiply the 8 argb u16s with the 8 alpha u16s
+			argb_vec = _mm_mulhi_epu16(argb_vec, alpha_vec);
+
+			// Put the low bytes of each argb u16 into the destination buffer
+			argb_vec = _mm_packus_epi16(argb_vec, dummy_vec);
+			_mm_storeu_si64(dest_bytes + index, argb_vec);
+		}
+	}
+}
+
+#else
+#define set_alpha set_alpha_slow
+
+static void set_alpha_slow(uint32_t *orig, struct pool_buffer *buf, float alpha) {
 	for (size_t y = 0; y < buf->height; ++y) {
 		for (size_t x = 0; x < buf->width; ++x) {
 			size_t index = y * buf->width + x;
@@ -28,6 +71,8 @@ static void set_alpha(uint32_t *orig, struct pool_buffer *buf, float alpha) {
 		}
 	}
 }
+
+#endif
 
 void fade_prepare(struct swaylock_fade *fade, struct pool_buffer *buffer) {
 	if (!fade->target_time) {
@@ -59,7 +104,19 @@ void fade_update(struct swaylock_fade *fade, struct pool_buffer *buffer, uint32_
 	}
 
 	double alpha = (double)fade->current_time / (double)fade->target_time;
+
+#ifdef FADE_PROFILE
+	double before = get_time();
+#endif
+
 	set_alpha(fade->original_buffer, buffer, alpha);
+
+#ifdef FADE_PROFILE
+	double after = get_time();
+	printf("set alpha in %fms (%fFPS). %fms since last time, FPS: %f\n",
+			(after - before) * 1000, 1 / (after - before),
+			delta, 1000 / delta);
+#endif
 }
 
 bool fade_is_complete(struct swaylock_fade *fade) {
