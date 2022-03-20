0acbfa234014c6e2a6e747f8a1b011f602733ae0
Johan Malm <jgm323@gmail.com>
2022-03-20 21:07:10 +0000

    cairo.h: remove redundant prototype
    cairo_surface_t *cairo_image_surface_scale();

--- include/cairo.h
+++ include/cairo.h
@@ -12,9 +12,6 @@
 void cairo_set_source_u32(cairo_t *cairo, uint32_t color);
 cairo_subpixel_order_t to_cairo_subpixel_order(enum wl_output_subpixel subpixel);
 
-cairo_surface_t *cairo_image_surface_scale(cairo_surface_t *image,
-		int width, int height);
-
 #if HAVE_GDK_PIXBUF
 
 cairo_surface_t* gdk_cairo_image_surface_create_from_pixbuf(
