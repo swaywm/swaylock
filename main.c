2dc3e52d7f6c47f2c99c5075d4b6dc2f1957b4b5
sneezycat <sneezycat@mail.com>
2026-05-23 12:36:08 +0200

    Add caps-lock-separator-color

--- main.c
+++ main.c
@@ -655,7 +655,7 @@ static int parse_options(int argc, char **argv, struct swaylock_state *state,
 			"Sets the color of the ring of the indicator when invalid.\n"
 		"  --separator-color <color>        "
 			"Sets the color of the lines that separate highlight segments.\n"
-		"  --caps-lock-sep-color <color>        "
+		"  --caps-lock-sep-color <color>    "
 			"Sets the color of the lines that separate highlight segments when"
 			"Caps Lock is active.\n"
 		"  --text-color <color>             "
